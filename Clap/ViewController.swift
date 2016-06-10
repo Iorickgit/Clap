ba01cad0c97682c720c24e28bd38c9f224e48d63
Iori Minami <iori.minami@gmail.com>
2016-06-11 00:05:06 +0900

    re-push

--- Clap/ViewController.swift
+++ Clap/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var clapPickerView: UIPickerView!
+    var audioPlayer: AVAudioPlayer!
+    var soundCount: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clapPickerView.delegate = self
+        clapPickerView.dataSource = self
+        let soundFilePath = NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
+        let fileURL = NSURL(fileURLWithPath: soundFilePath)
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOfURL: fileURL)
+        }catch{
+            print("音楽ファイルが読み込めませんでした")
+        }
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView,  numberOfRowsInComponent component: Int) -> Int!{
+        return 10
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return "\(row+1)回"
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        soundCount = row
+    }
+    
+    @IBAction func playbutton(){
+        audioPlayer.numberOfLoops = soundCount
+        audioPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
